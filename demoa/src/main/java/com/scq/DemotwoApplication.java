73aa6abf5a4cc2d8b79c120de917f543acfb7ced
Administrator <13521736975@163.com>
2021-02-09 18:29:12 +0800

    2021/02/09:SpringBoot框架

---
+++ demoa/src/main/java/com/scq/DemotwoApplication.java
@@ -0,0 +1,13 @@
+package com.scq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotwoApplication.class, args);
+	}
+
+}
